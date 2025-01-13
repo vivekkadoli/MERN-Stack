ed02be65f9f28d89c5479cc9756fb0146fa7bcbd
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 11:52:07 +0530

    Starting with class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const App = () => {
-  return <h1> Welcome to ReactJS </h1>;
-};
+// const App = () => {
+//   return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
+// };
 
 export default App;
