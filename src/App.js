adf2a2c139e73cd953e65e0634e177f239fef2c8
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 13:41:52 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+
+import CourseList from "./components/CourseList";
+
 import './App.css';
 const App = () => {
   return <div className="course-goals">
     <h2>MERN Stack Course</h2>
-    <ul className="course-list">
-      <li>Nodejs &amp; ReactJS</li>
-      <li>Git</li>
-      <li>ExpressJs &amp; MongoDB</li>
-    </ul>
+    <CourseList/>
   </div>
   //<h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
 };
---
+++ src/components/CourseList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CourseList = () => {
+    return (
+        <ul className="course-list">
+            <li>Nodejs &amp; ReactJS</li>
+            <li>Git</li>
+            <li>ExpressJs &amp; MongoDB</li>
+        </ul>
+    );
+};
+
+export default CourseList;
