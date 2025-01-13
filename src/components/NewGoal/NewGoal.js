ed50f8e6e822172cba6fd05d5de746194db1c8dd
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 18:08:41 +0530

    Changes done

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -3,7 +3,18 @@ import React from "react";
 import "./NewGoal.css";
 
 const NewGoal = () => {
-    return <form className="new-goal">
+    const addGoalHandler = event => {
+        event.preventDefault();
+
+        const newGoal = {
+            id: Math.random().toString(),
+            text: 'Dumy Goal'
+        };
+
+        console.log(newGoal);
+    };
+
+    return <form className="new-goal" onSubmit={addGoalHandler}>
         <input type="text"/>
         <button type="submit">Add Goal</button>
     </form>
