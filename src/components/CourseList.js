866e9a164ca703ab12e97911c1c50243f12217f0
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 17:44:54 +0530

    Changes done

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -4,7 +4,11 @@ import './CourseList.css';
 
 const CourseList = props => {
     console.log(props.goals)
-    return <ul className="course-list">{[<li>Hi</li>, <li>This works</li>]}</ul>;
+    return <ul className="course-list">{
+        props.goals.map((goal) => {
+            return <li key={goal.id}>{goal.text}</li>;
+        })
+    }</ul>;
 };
 
 export default CourseList;
