c9a5a868954769dde7438f5d6a4c938b4705a81f
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 13:29:38 +0530

    modified code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 const App = () => {
-  return <div>
+  return <div className="course-goals">
     <h2>MERN Stack Course</h2>
     <ul className="course-list">
       <li>Nodejs &amp; ReactJS</li>
