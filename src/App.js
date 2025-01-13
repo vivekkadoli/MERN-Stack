1a15aa7c9f72f0b869dbe78b66eae02c6d551c04
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 12:20:02 +0530

    Developed Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
+  return <div>
+    <h2>Nodejs</h2>
+    <ul>
+      <li>ReactJS</li>
+      <li>Git</li>
+      <li>ExpressJs &amp; MongoDB (MERN Stack)</li>
+    </ul>
+  </div>
+  //<h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
 };
 
 export default App;
