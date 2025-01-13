769da4d7961a95489dbc446044f43f9f21647f39
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 11:55:06 +0530

    Declared Class

--- src/App.js
+++ src/App.js
@@ -4,4 +4,10 @@ import React from "react";
 //   return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
 // };
 
+class App extends React.Component {
+  render() {
+    return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
+  }
+}
+
 export default App;
