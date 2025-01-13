77237ca7bd171429754c1d9d2b7606c7ef270eb3
vivekkadoli <vivekkadoli@gmail.com>
2025-01-13 11:57:45 +0530

    Reversed all class was for example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-// const App = () => {
-//   return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
-// };
+const App = () => {
+  return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
+};
 
-class App extends React.Component {
-  render() {
-    return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <h1> Welcome to ReactJS with <span>Git and Nodejs</span></h1>;
+//   }
+// }
 
 export default App;
