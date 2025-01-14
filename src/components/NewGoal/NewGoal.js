45487e94116974cf1a09a09344f39b2f25c8e683
vivekkadoli <vivekkadoli@gmail.com>
2025-01-14 12:16:27 +0530

    Modified NewGoal.js removed React library

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./NewGoal.css";
 
 const NewGoal = props => {
+    let enteredText = '';
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'Dumy Goal'
+            text: enteredText
         };
+
+        console.log(newGoal);
 
         props.onAddGoal(newGoal);
     };
 
-    return <form className="new-goal" onSubmit={addGoalHandler}>
-        <input type="text"/>
-        <button type="submit">Add Goal</button>
-    </form>
+    const textChangeHandler = event => {
+       enteredText = event.target.value;
+    };
+
+    return (
+        <form className="new-goal" onSubmit={addGoalHandler}>
+            <input type="text" onChange={textChangeHandler}/>
+            <button type="submit">Add Goal</button>
+        </form>
+    );
 };
 
 export default NewGoal;
