08bd6f35de3a53c6a7af758291cd68ea455a16a9
vivekkadoli <vivekkadoli@gmail.com>
2025-01-17 20:09:31 +0530

    New code files added and modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
-  return ( 
+  return (
     <Router>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
-          <Users/>
+          <Users />
         </Route>
         <Route path="/places/new" exact>
-          <NewPlace/>
+          <NewPlace />
         </Route>
-        <Redirect to="/"/>
+        <Redirect to="/" />
       </Switch>
     </Router>
   );
