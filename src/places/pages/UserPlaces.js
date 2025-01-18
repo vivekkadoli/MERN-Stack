45d74366055af0a64fce698f039d1fa198f8be84
vivekkadoli <vivekkadoli@gmail.com>
2025-01-18 17:51:01 +0530

    Rendering User Places & Using Dynamic Route Segments

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Gateway of India",
+    description:
+      "The Gateway of India is a 26-meter (85-foot) arch monument in Mumbai, India that's a top tourist attraction and symbol of the city's history and independence",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSHQujztb-O86piW_-DaD8n4HUMyI2ZH5tcLSFZoqWzM5REv6Z_DJteZNG5Oibcrfbutg&usqp=CAU",
+    address: "Apollo Bandar, Colaba, Mumbai, Maharashtra 400001",
+    location: {
+      lat: 18.9219841,
+      lng: 72.8320794,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Shrimant Dagdusheth Halwai Ganapati Temple",
+    description:
+      "Hindu temple popular with pilgrims, with marble interiors & a golden Ganesh idol.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipMWovqKOLvIGdrhhcahVWVNNcyIQaYOVhxjTd6G=w408-h306-k-no",
+    address:
+      "Ganpati Bhavan, 250, Chhatrapati Shivaji Maharaj Rd, Mehunpura, Budhwar Peth, Pune, Maharashtra 411002",
+    location: {
+      lat: 18.5164297,
+      lng: 73.853558,
+    },
+    creator: "u2",
+  },
+];
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Max Schwarz",
+      name: "Vivek Kadoli",
       image:
         "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8=",
       places: 3,
