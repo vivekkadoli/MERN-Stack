3d53f6eb8f6420e1e4f753098888fd11a6778d17
vivekkadoli <vivekkadoli@gmail.com>
2025-01-18 18:06:19 +0530

    Added Button styles

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElement/Button";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
@@ -15,9 +16,9 @@ const PlaceItem = (props) => {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <button>View on Map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button inverse>View on Map</Button>
+          <Button to={`/places/${props.id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
