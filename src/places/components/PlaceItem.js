8c676f6a6c7f4cff0a2a3e133e7c3f645965b372
vivekkadoli <vivekkadoli@gmail.com>
2025-01-20 13:42:59 +0530

    Adding a Custom Form Input Component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
-import Button from "../../shared/components/FormElement/Button";
+import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import Map from "../../shared/components/UIElements/Map";
 import "./PlaceItem.css";
