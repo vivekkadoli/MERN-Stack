4c2b22834761eff8b28cc3e63d07d202de2438c8
vivekkadoli <vivekkadoli@gmail.com>
2025-01-21 14:27:55 +0530

    Adding Signup and 'Switch Mode' Button (I have fixed some errors too)

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -8,7 +8,7 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: validate(action.val, action.validators),
+        isValid: validate(action.val, action.validators || []),
       };
     case "TOUCH":
       return {
