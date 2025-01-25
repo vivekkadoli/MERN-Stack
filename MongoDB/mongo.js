c820a6b6cee24e7bca99b18f872e4accacd67a16
vivekkadoli <vivekkadoli@gmail.com>
2025-01-25 15:54:48 +0530

    Creating a Simple Backend & Connecting it to the Database

---
+++ MongoDB/mongo.js
@@ -0,0 +1,11 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://MERN_Stack:YhoeVyJpLS77ku8e@cluster0.0r089.mongodb.net/places_test?retryWrites=true&w=majority&appName=Cluster0";
+
+const createProduct = async (req, res, next) => {};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
