5528732d0dec87441a57b7cc272981f8e5c8551f
vivekkadoli <vivekkadoli@gmail.com>
2025-01-29 18:09:29 +0530

    Improving the Custom Http Hook

--- FrontEnd/src/shared/hooks/http-hook.js
+++ FrontEnd/src/shared/hooks/http-hook.js
@@ -22,15 +22,20 @@ export const useHttpClient = () => {
 
         const responseData = await response.json();
 
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
@@ -41,7 +46,7 @@ export const useHttpClient = () => {
 
   useEffect(() => {
     return () => {
-      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abortCtrl());
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
     };
   }, []);
 
