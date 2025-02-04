c3678b17cd50a4d71e6967d28dac6bc796edf997
vivekkadoli <vivekkadoli@gmail.com>
2025-02-04 20:10:59 +0530

    Filtering files on the backend (Images Only!)

--- BackEnd/routes/users-routes.js
+++ BackEnd/routes/users-routes.js
@@ -4,12 +4,15 @@ const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controller");
 
+const fileUpload = require("../middleware/file-upload");
+
 const router = express.Router();
 
 router.get("/", usersController.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
