e6ca01a52747a5dbf69e97a846a6ae93fbbd1b6d
vivekkadoli <vivekkadoli@gmail.com>
2025-02-06 20:22:28 +0530

    Backend Route Protection with Auth Middleware

--- BackEnd/routes/places-route.js
+++ BackEnd/routes/places-route.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
 const { check } = require("express-validator");
-
 const placesController = require("../controllers/places-controller");
-
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/:pid", placesController.getPlaceByID);
 
 router.get("/user/:uid", placesController.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post(
   "/",
