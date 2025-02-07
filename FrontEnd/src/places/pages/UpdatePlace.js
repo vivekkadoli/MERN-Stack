ddc97a0cd736d472417a61aedfae0ee9cb43d62c
vivekkadoli <vivekkadoli@gmail.com>
2025-02-07 09:51:02 +0530

    Using Tokens to Update and Delete Places

--- FrontEnd/src/places/pages/UpdatePlace.js
+++ FrontEnd/src/places/pages/UpdatePlace.js
@@ -63,6 +63,7 @@ const UpdatePlace = () => {
         }),
         {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
         }
       );
       history.push("/" + auth.userId + "/places");
