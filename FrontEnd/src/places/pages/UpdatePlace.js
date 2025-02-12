3e1c6dad9feba07c09ab7993b3f993b98c358d80
vivekkadoli <vivekkadoli@gmail.com>
2025-02-12 18:03:44 +0530

    Environment Variables in React Apps

--- FrontEnd/src/places/pages/UpdatePlace.js
+++ FrontEnd/src/places/pages/UpdatePlace.js
@@ -33,7 +33,7 @@ const UpdatePlace = () => {
     const fetchPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
         setLoadedPlaces(responseData.place);
         setFormData(
@@ -55,7 +55,7 @@ const UpdatePlace = () => {
     event.preventDefault();
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         "PATCH",
         JSON.stringify({
           title: formState.inputs.title.value,
